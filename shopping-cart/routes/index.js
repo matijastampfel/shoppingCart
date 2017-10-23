4fb4f288b9d065314b57be9708e0e525a6c1ec6c
matijastampfel <matija.stampfel@gmail.com>
2017-10-23 20:10:48 +0200

    Cart Model

--- shopping-cart/routes/index.js
+++ shopping-cart/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+var Cart = require('../models/cart');
 var Product = require('../models/product');
 
 
@@ -13,6 +14,21 @@ router.get('/', function(req, res, next) {
       productsChunks.push(docs.slice(i, i + chunkSize));
     }
     res.render('shop/index', { title: 'Shopping Cart', products: productsChunks });
+  });
+});
+
+router.get('/add-to-cart/:id', function(req, res, next){
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  Product.findById(function(err, product){
+    if(err){
+      return res.redirect('/');
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect('/');
   });
 });
 
