903ebe136e00ca071a38b7d6b23a8bda823950c4
matijastampfel <matija.stampfel@gmail.com>
2017-10-24 22:03:53 +0200

    Cart model

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -45,7 +45,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req,res,next){
+app.use(function(req, res, next){
   res.locals.login = req.isAuthenticated();
   res.locals.session = req.session;
   next();
--- shopping-cart/routes/index.js
+++ shopping-cart/routes/index.js
@@ -21,7 +21,7 @@ router.get('/add-to-cart/:id', function(req, res, next){
   var productId = req.params.id;
   var cart = new Cart(req.session.cart ? req.session.cart : {});
 
-  Product.findById(function(err, product){
+  Product.findById(productId, function(err, product){
     if(err){
       return res.redirect('/');
     }
@@ -30,6 +30,14 @@ router.get('/add-to-cart/:id', function(req, res, next){
     console.log(req.session.cart);
     res.redirect('/');
   });
+});
+
+router.get('/shopping-cart', function(req, res, next){
+  if(!req.session.cart) {
+    return res.render('shop/shopping-cart', {products: null});
+  }
+    var cart = new Cart(req.session.cart);
+    res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice} );
 });
 
 module.exports = router;
