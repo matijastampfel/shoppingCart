803da430d83980b5aea0c7382cac8c9d3d286ac9
matijastampfel <matija.stampfel@gmail.com>
2017-10-25 21:51:28 +0200

    Forcing User Login

--- shopping-cart/routes/user.js
+++ shopping-cart/routes/user.js
@@ -29,7 +29,15 @@ router.post('/signup', passport.authenticate('local.signup', {
   successRedirect:'/user/profile',
   failureRedirect:'/user/signup',
   failureFlash: true
-}));
+}), function(req,res,next){
+  if(req.session.oldUrl){
+    var oldUrl = req.session.oldUrl;
+    req.session.oldUrl = null;
+    res.redirect(oldUrl);
+  } else {
+    res.redirect('/user/profile')
+  }
+});
 
 
 
@@ -40,10 +48,17 @@ router.get('/signin',function(req,res,next) {
 
 
 router.post('/signin', passport.authenticate('local.signin', {
-  successRedirect:'/user/profile',
   failureRedirect:'/user/signin',
   failureFlash: true
-}));
+}), function(req,res,next){
+      if(req.session.oldUrl){
+        var oldUrl = req.session.oldUrl;
+        req.session.oldUrl = null;
+        res.redirect(oldUrl);
+      } else {
+        res.redirect('/user/profile')
+      }
+});
 
 
 
